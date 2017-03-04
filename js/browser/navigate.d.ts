c65f75c562929baac4e6056a95fbabd2ed4906c0
dx1 <dx1alex@gmail.com>
2017-03-04 21:28:08 +0700

    v1.2.1

--- js/browser/navigate.d.ts
+++ js/browser/navigate.d.ts
@@ -3,7 +3,7 @@ import { Utils } from './utils';
 export interface Navigate extends Utils {
 }
 export declare class Navigate extends Base {
-    url(url: string, pause?: number): Promise<string>;
+    url(url?: string, pause?: number): Promise<string>;
     go(url: string, pause?: number): Promise<void>;
     refresh(pause?: number): Promise<void>;
     back(pause?: number): Promise<void>;
